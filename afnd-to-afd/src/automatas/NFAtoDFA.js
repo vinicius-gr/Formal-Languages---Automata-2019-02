927a2b9de3ea9442cef9844f41975492282d6087
Bomfim <mariocesarbomfim@gmail.com>
2021-04-20 17:49:22 -0300

    fix

--- afnd-to-afd/src/automatas/NFAtoDFA.js
+++ afnd-to-afd/src/automatas/NFAtoDFA.js
@@ -2,77 +2,87 @@
 
 import DeterministcFiniteAutomata from "./DFA.js";
 
-function parseNFA({
-    states,
-    alphabet,
-    transitions,
-    start,
-    acceptanceStates
-}) {
+function parseNFA({ states, alphabet, transitions, start, acceptanceStates }) {
+  let parsedStates = new Set();
+  let parsedTransitions = {};
+  let parsedacceptanceStates = [];
+  parsedStates.add(start);
 
-    let parsedStates = new Set();
-    let parsedTransitions = {};
-    let parsedacceptanceStates = [];
-    parsedStates.add(start);
-
-    parsedStates.forEach((state) => {
-        console.log("\nExpanding state=",state)
-        alphabet.split('').map((symbol) => {
-            parsedTransitions[state] = parsedTransitions[state] ? parsedTransitions[state] : {};
-            console.log("State="+state+"\nParsed transitions=",parsedTransitions[state]);
-            if (state.includes('_')) {
-                let set = new Set();
-                state.split('_').forEach(sub_state => {
-                    if (transitions[sub_state][symbol]) {
-                        if (Array.isArray(transitions[sub_state][symbol]))
-                            transitions[sub_state][symbol].map((v) => (set.add(v)));
-                        else
-                            set.add(transitions[sub_state][symbol]);
-                    }
-                });
-                parsedTransitions[state][symbol] = Array.from(set).sort().join('_');
-                console.log("State="+state+"\nParsed transitions=",parsedTransitions[state]);
-            } else if (transitions[state][symbol]) {
-                //verifica se a transição é deterministica
-                if (Array.isArray(transitions[state][symbol])) {
-                    //concatena a transição nao deterministica
-                    parsedTransitions[state][symbol] = transitions[state][symbol].join('_');
-                    console.log("New State=", parsedTransitions[state][symbol])
-                } else {
-                    parsedTransitions[state][symbol] = transitions[state][symbol];
-                }
-            }
-            //adiciona o estado relacionado a transição convertida na lista de estados convertidos
-            if (parsedTransitions[state][symbol] && !parsedStates.has(parsedTransitions[state][symbol]))
-                parsedStates.add(parsedTransitions[state][symbol]);
+  parsedStates.forEach((state) => {
+    console.log("\nExpanding state=", state);
+    alphabet.split("").map((symbol) => {
+      parsedTransitions[state] = parsedTransitions[state]
+        ? parsedTransitions[state]
+        : {};
+      console.log(
+        "State=" + state + "\nParsed transitions=",
+        parsedTransitions[state]
+      );
+      if (state.includes("_")) {
+        let set = new Set();
+        state.split("_").forEach((sub_state) => {
+          if (transitions[sub_state][symbol]) {
+            if (Array.isArray(transitions[sub_state][symbol]))
+              transitions[sub_state][symbol].map((v) => set.add(v));
+            else set.add(transitions[sub_state][symbol]);
+          }
         });
+        parsedTransitions[state][symbol] = Array.from(set).sort().join("_");
+        console.log(
+          "State=" + state + "\nParsed transitions=",
+          parsedTransitions[state]
+        );
+      } else if (
+        transitions[state] &&
+        transitions[state].hasOwnProperty(symbol) &&
+        transitions[state][symbol]
+      ) {
+        //verifica se a transição é deterministica
+        if (Array.isArray(transitions[state][symbol])) {
+          //concatena a transição nao deterministica
+          parsedTransitions[state][symbol] = transitions[state][symbol].join(
+            "_"
+          );
+          console.log("New State=", parsedTransitions[state][symbol]);
+        } else {
+          parsedTransitions[state][symbol] = transitions[state][symbol];
+        }
+      }
+      //adiciona o estado relacionado a transição convertida na lista de estados convertidos
+      if (
+        transitions[state] &&
+        transitions[state].hasOwnProperty(symbol) &&
+        parsedTransitions[state][symbol] &&
+        !parsedStates.has(parsedTransitions[state][symbol])
+      )
+        parsedStates.add(parsedTransitions[state][symbol]);
     });
+  });
 
-    parsedStates.forEach((state) => {
-        if (state.includes(acceptanceStates)) parsedacceptanceStates.push(state)
-    });
+  parsedStates.forEach((state) => {
+    if (state.includes(acceptanceStates)) parsedacceptanceStates.push(state);
+  });
 
-    return {
-        parsedStates,
-        parsedTransitions,
-        start,
-        parsedacceptanceStates
-    }
+  return {
+    parsedStates,
+    parsedTransitions,
+    start,
+    parsedacceptanceStates,
+  };
 }
 
-
 export default function toDFA(NFA) {
-    const {
-        parsedStates,
-        parsedTransitions,
-        start,
-        parsedacceptanceStates
-    } = parseNFA(NFA);
-    return new DeterministcFiniteAutomata({
-        states: Array.from(parsedStates),
-        alphabet: NFA.alphabet,
-        transitions: parsedTransitions,
-        start: start,
-        acceptanceStates: parsedacceptanceStates
-    });
-};
+  const {
+    parsedStates,
+    parsedTransitions,
+    start,
+    parsedacceptanceStates,
+  } = parseNFA(NFA);
+  return new DeterministcFiniteAutomata({
+    states: Array.from(parsedStates),
+    alphabet: NFA.alphabet,
+    transitions: parsedTransitions,
+    start: start,
+    acceptanceStates: parsedacceptanceStates,
+  });
+}
